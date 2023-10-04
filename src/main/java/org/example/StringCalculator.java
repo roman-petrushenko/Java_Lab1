3083e0575ecd06806e48fc50a76a4f5c1b40eee0
Roman <roman.petrushenko1937@gmail.com>
2023-10-04 21:53:02 +0300

    Step 3

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -4,6 +4,7 @@ public class StringCalculator {
     public int add(String s1) {
         int r = 0;
         if (s1.isEmpty() == true) return r;
+        s1 = s1.replace("/n",",");
         String[] arr1;
         arr1 = s1.split(",");
         for (int i = 0; i<arr1.length; i+=1){
