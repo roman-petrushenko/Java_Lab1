c57eddec8035e7bd9bfcfdd2561f4bfdb1fae264
Roman <roman.petrushenko1937@gmail.com>
2023-10-05 01:14:08 +0300

    Step 4

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -6,7 +6,15 @@ public class StringCalculator {
         if (s1.isEmpty() == true) return r;
         s1 = s1.replace("/n",",");
         String[] arr1;
-        arr1 = s1.split(",");
+        if (s1.startsWith("//")){
+            String regex1 = s1.substring(2, s1.indexOf(","));
+                int k = s1.indexOf(",")+1;
+                s1 = s1.substring(k);
+                if (s1.isEmpty() == true) return r;
+                s1 = s1.replace(regex1,",");
+                arr1 = s1.split(",");}
+        else {arr1 = s1.split(",");}
+
         for (int i = 0; i<arr1.length; i+=1){
             r+=Integer.parseInt(arr1[i]);
         }
