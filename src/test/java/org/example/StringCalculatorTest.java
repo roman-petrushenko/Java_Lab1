fe697862d549f8e389256fc8c8ec9145e4e8edc4
Roman <roman.petrushenko1937@gmail.com>
2023-10-07 09:52:55 +0300

    Step 9.2

--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -26,37 +26,37 @@ class StringCalculatorTest {
     @Test
     void Test3() {
         StringCalculator string_calc1 = new StringCalculator();
-        assertEquals(-1, string_calc1.add("2,5\n-100,-2"));
+        assertEquals(-1, string_calc1.add("2,5\\n-100,-2"));
     }
 
     @Test
     void Test4() {
         StringCalculator string_calc1 = new StringCalculator();
-        assertEquals(107, string_calc1.add("2,5\n0,100"));
+        assertEquals(107, string_calc1.add("2,5\\n0,100"));
     }
 
     @Test
     void Test5() {
         StringCalculator string_calc1 = new StringCalculator();
-        assertEquals(1028, string_calc1.add("//[***][*][**]\n9**2,1000\n5*10***2"));
+        assertEquals(1028, string_calc1.add("//[***][*][**]\\n9**2,1000\\n5*10***2"));
     }
 
     @Test
     void Test6() {
         StringCalculator string_calc1 = new StringCalculator();
-        assertEquals(1029, string_calc1.add("//[***][*][**]\n9**2,1000\n5*10***2,1001\n0,1"));
+        assertEquals(1029, string_calc1.add("//[***][*][**]\\n9**2,1000\\n5*10***2,1001\\n0,1"));
     }
 
     @Test
     void Test7() {
         StringCalculator string_calc1 = new StringCalculator();
-        assertEquals(1029, string_calc1.add("//[***][*][**]\n9**2,1000\n5*10***2,1001\n0,1"));
+        assertEquals(1029, string_calc1.add("//[***][*][**]\\n9**2,1000\\n5*10***2,1001\\n0,1"));
     }
 
     @Test
     void Test8() {
         StringCalculator string_calc1 = new StringCalculator();
-        assertEquals(0, string_calc1.add("//[***][*][**]\n"));
+        assertEquals(0, string_calc1.add("//[***][*][**]\\n"));
     }
     @Test
     void Test9() {
@@ -66,7 +66,7 @@ class StringCalculatorTest {
     @Test
     void Test10() {
         StringCalculator string_calc1 = new StringCalculator();
-        assertEquals(108, string_calc1.add("//[k]\n3k100,1010k1\n4"));
+        assertEquals(108, string_calc1.add("//[k]\\n3k100,1010k1\\n4"));
     }
 }
 
